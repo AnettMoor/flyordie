d8533383cf942f267108526aad41f18835da39e9
AnettMoor <anettmoor@gmail.com>
2025-10-27 12:22:48 +0200

    revert to previous

--- client.py
+++ client.py
@@ -1,9 +1,7 @@
+import network, socket, time
 import machine
-import network
-import socket
-import time
 
-# --- Nupud ja LEDid ---
+
 nupp_tagasi = machine.Pin(0, machine.Pin.IN, machine.Pin.PULL_DOWN)
 led_tagasi = machine.Pin(1, machine.Pin.OUT)
 
@@ -16,27 +14,8 @@ led_vasakule = machine.Pin(15, machine.Pin.OUT)
 nupp_paremale = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_DOWN)
 led_paremale = machine.Pin(18, machine.Pin.OUT)
 
-# --- Mootorid (ühendatud läbi transistoride või draiveri) ---
-mootor_edasi_vasak = machine.Pin(2, machine.Pin.OUT)
-mootor_edasi_parem = machine.Pin(3, machine.Pin.OUT)
-mootor_tagasi_vasak = machine.Pin(4, machine.Pin.OUT)
-mootor_tagasi_parem = machine.Pin(5, machine.Pin.OUT)
 
-# --- Mootori parameetrid ---
-KV = 19000       # pööret volt/sek
-PINGE = 3.7      # toitepinge
-porded_sekundis = KV * PINGE / 60  # RPM → RPS (pööret sekundis)
 
-def keera_mootorit(mootor, poorded):
-    """Käivitab ühe mootori määratud arv pöördeid"""
-    kestus = poorded / porded_sekundis
-    print("Mootor teeb", poorded, "pööret (~", round(kestus, 4), "sekundit )")
-    mootor.value(1)
-    time.sleep(kestus)
-    mootor.value(0)
-    print("Mootor peatatud.")
-
-# --- WiFi ühendus ---
 ssid = "Galaxy S20 FE"
 password = "flyordie"
 
@@ -46,26 +25,24 @@ wlan.connect(ssid, password)
 while not wlan.isconnected():
     time.sleep(0.5)
 
-server_ip = "10.169.113.65"
+server_ip = "10.169.113.65" # server ip
 s = socket.socket()
 s.connect((server_ip, 1234))
 s.setblocking(False)
 print("Ühendatud serveriga:", server_ip)
 
-# --- Muutujad eelmise seisundi jälgimiseks ---
-prev_edasi = prev_tagasi = prev_vasakule = prev_paremale = 0
+prev_edasi = 0
+prev_tagasi = 0
+prev_vasakule = 0
+prev_paremale = 0
 
-# --- Peatsükliline töö ---
 while True:
-    # --- Edasi ---
+    # edasi
     if nupp_edasi.value() == 1:
         led_edasi.value(1)
         if prev_edasi == 0:
             s.send(b"BTN_EDASI_ON\n")
             print("Saadetud: BTN_EDASI_ON")
-            # Käivita mõlemad esimesed mootorid 50 pööret
-            keera_mootorit(mootor_edasi_vasak, 50)
-            keera_mootorit(mootor_edasi_parem, 50)
         prev_edasi = 1
     else:
         led_edasi.value(0)
@@ -74,14 +51,12 @@ while True:
             print("Saadetud: BTN_EDASI_OFF")
         prev_edasi = 0
 
-    # --- Tagasi ---
+    # tagasi
     if nupp_tagasi.value() == 1:
         led_tagasi.value(1)
         if prev_tagasi == 0:
             s.send(b"BTN_TAGASI_ON\n")
             print("Saadetud: BTN_TAGASI_ON")
-            keera_mootorit(mootor_tagasi_vasak, 50)
-            keera_mootorit(mootor_tagasi_parem, 50)
         prev_tagasi = 1
     else:
         led_tagasi.value(0)
@@ -90,15 +65,12 @@ while True:
             print("Saadetud: BTN_TAGASI_OFF")
         prev_tagasi = 0
 
-    # --- Vasakule ---
+    # vasakule
     if nupp_vasakule.value() == 1:
         led_vasakule.value(1)
         if prev_vasakule == 0:
             s.send(b"BTN_VASAKULE_ON\n")
             print("Saadetud: BTN_VASAKULE_ON")
-            # vasakule pööramiseks käivitame vastavad mootorid
-            keera_mootorit(mootor_edasi_parem, 30)
-            keera_mootorit(mootor_tagasi_vasak, 30)
         prev_vasakule = 1
     else:
         led_vasakule.value(0)
@@ -107,14 +79,12 @@ while True:
             print("Saadetud: BTN_VASAKULE_OFF")
         prev_vasakule = 0
 
-    # --- Paremale ---
+    # paremale
     if nupp_paremale.value() == 1:
         led_paremale.value(1)
         if prev_paremale == 0:
             s.send(b"BTN_PAREMALE_ON\n")
             print("Saadetud: BTN_PAREMALE_ON")
-            keera_mootorit(mootor_edasi_vasak, 30)
-            keera_mootorit(mootor_tagasi_parem, 30)
         prev_paremale = 1
     else:
         led_paremale.value(0)
@@ -123,7 +93,7 @@ while True:
             print("Saadetud: BTN_PAREMALE_OFF")
         prev_paremale = 0
 
-    # --- Serveri vastus ---
+    # serveri vastus
     try:
         data = s.recv(1024)
         if data:
@@ -131,4 +101,4 @@ while True:
     except:
         pass
 
-    time.sleep(0.1)
+    time.sleep(0.2)
