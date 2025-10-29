c8dd5262f3e02dcf44eacff137deaa072f324a77
AnettMoor <anettmoor@gmail.com>
2025-10-29 12:54:06 +0200

    remote buttons, pins correct

--- client.py
+++ client.py
@@ -1,19 +1,10 @@
 import network, socket, time
 import machine
 
-
-nupp_tagasi = machine.Pin(0, machine.Pin.IN, machine.Pin.PULL_DOWN)
-led_tagasi = machine.Pin(1, machine.Pin.OUT)
-
-nupp_edasi = machine.Pin(17, machine.Pin.IN, machine.Pin.PULL_DOWN)
-led_edasi = machine.Pin(16, machine.Pin.OUT)
-
-nupp_vasakule = machine.Pin(13, machine.Pin.IN, machine.Pin.PULL_DOWN)
-led_vasakule = machine.Pin(15, machine.Pin.OUT)
-
-nupp_paremale = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_DOWN)
-led_paremale = machine.Pin(18, machine.Pin.OUT)
-
+nupp_edasi = machine.Pin(16, machine.Pin.IN, machine.Pin.PULL_DOWN)
+nupp_tagasi = machine.Pin(27, machine.Pin.IN, machine.Pin.PULL_DOWN)
+nupp_vasakule = machine.Pin(20, machine.Pin.IN, machine.Pin.PULL_DOWN)
+nupp_paremale = machine.Pin(18, machine.Pin.IN, machine.Pin.PULL_DOWN)
 
 
 ssid = "Galaxy S20 FE"
@@ -39,13 +30,11 @@ prev_paremale = 0
 while True:
     # edasi
     if nupp_edasi.value() == 1:
-        led_edasi.value(1)
         if prev_edasi == 0:
             s.send(b"BTN_EDASI_ON\n")
             print("Saadetud: BTN_EDASI_ON")
         prev_edasi = 1
     else:
-        led_edasi.value(0)
         if prev_edasi == 1:
             s.send(b"BTN_EDASI_OFF\n")
             print("Saadetud: BTN_EDASI_OFF")
@@ -53,13 +42,11 @@ while True:
 
     # tagasi
     if nupp_tagasi.value() == 1:
-        led_tagasi.value(1)
         if prev_tagasi == 0:
             s.send(b"BTN_TAGASI_ON\n")
             print("Saadetud: BTN_TAGASI_ON")
         prev_tagasi = 1
     else:
-        led_tagasi.value(0)
         if prev_tagasi == 1:
             s.send(b"BTN_TAGASI_OFF\n")
             print("Saadetud: BTN_TAGASI_OFF")
@@ -67,13 +54,11 @@ while True:
 
     # vasakule
     if nupp_vasakule.value() == 1:
-        led_vasakule.value(1)
         if prev_vasakule == 0:
             s.send(b"BTN_VASAKULE_ON\n")
             print("Saadetud: BTN_VASAKULE_ON")
         prev_vasakule = 1
     else:
-        led_vasakule.value(0)
         if prev_vasakule == 1:
             s.send(b"BTN_VASAKULE_OFF\n")
             print("Saadetud: BTN_VASAKULE_OFF")
@@ -81,13 +66,11 @@ while True:
 
     # paremale
     if nupp_paremale.value() == 1:
-        led_paremale.value(1)
         if prev_paremale == 0:
             s.send(b"BTN_PAREMALE_ON\n")
             print("Saadetud: BTN_PAREMALE_ON")
         prev_paremale = 1
     else:
-        led_paremale.value(0)
         if prev_paremale == 1:
             s.send(b"BTN_PAREMALE_OFF\n")
             print("Saadetud: BTN_PAREMALE_OFF")
